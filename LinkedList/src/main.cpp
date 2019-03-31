7067a61e0a68db0592c7ee5ae53555995cd18b9c
Mohamed Amn <mamn2@illinois.edu>
2019-03-31 16:21:01 -0500

    project structure update

---
+++ LinkedList/src/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  LinkedList
+//
+//  Created by Mohamed Amn on 3/31/19.
+//  Copyright © 2019 Mohamed Amn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
